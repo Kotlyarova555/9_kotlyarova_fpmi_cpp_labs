d76674824428d62a5f512ae5d13683d5fdddcae5
Sonya <sonyamailgomel@icloud.com>
2025-12-02 21:39:03 +0300

    Update

--- semester_1/lab4_matrices/lab4_matrices.cpp
+++ semester_1/lab4_matrices/lab4_matrices.cpp
@@ -7,8 +7,8 @@ void input_length(int& n){
     std::cout << "Введите число элементов матрицы" << std::endl;
     
     if (!(std::cin >> n) || n < 1) {
-        std::cout << "Ошибка: введите целое число" << std::endl;
-        std::exit(1);
+      throw std::string("oshibka, vvedite tseloe chislo");
+       
     }
 }
 
@@ -40,8 +40,8 @@ void input_random_borders(int& a, int& b){
     
  std::cout << "Введите рандомные границы a и b: " << std::endl;
  if(!(std::cin >> a >> b)) {
-    std::cout << "Ошибка: введите целые числа" << std::endl;
-    std::exit(1);;
+   throw std::string("oshibka, vvedite tselye chisla");
+  
  }
  if(a > b) {
    std::swap(a, b);
@@ -70,9 +70,10 @@ void manual_input(int** matrix, int n){
       for (int j = 0; j < n; j++) {
          std::cout << "[" << i << "][" << j << "]: " ;
          if(!(std::cin >> matrix[i][j])) {
-            std::cout << "Ошибка: vvedite целые числа" << std::endl;
+            throw std::string("oshibka, vvedite tselye chisla");
+            
             deleting_matrix(matrix, n);
-            std::exit(1);
+            
          
            }
         }
@@ -84,8 +85,8 @@ void choosing_type_of_input(int** matrix, int n, std::mt19937& gen){
 
  std::cout << "Выберите заполнение массива: Рандомно(1) или Вручную(2)" << std::endl;
  if(!(std::cin >> variant)) {
-      std::cout << "Ошибка " << std::endl;
-      std::exit(1);
+   throw std::string("oshibka, vvedite tseloe chislo") ;
+   
     }
    
  if(variant == 1){
@@ -96,8 +97,8 @@ std::cout << "Заполнение вручную:" << std::endl;
    manual_input(matrix, n);
    print_matrix(matrix, n);
  } else {
-    std::cout << "Ошибка: такого варинта нет " << std::endl;
-    exit(1);
+   throw std::string("Takogo varianra net");
+    
  } 
 }
 
@@ -171,6 +172,7 @@ void find_min_in_parallel_diagonals(int** matrix, int n) {
      }
   if (has_elements) {
    std::cout << "Диагональ ";
+   
    if(diag < 0) {
 
       std::cout << diag << " ";
@@ -193,6 +195,8 @@ void find_min_in_parallel_diagonals(int** matrix, int n) {
 
 int main(){
 int n;
+  
+  try {
 input_length(n);
 
 int** matrix;
@@ -208,5 +212,10 @@ sum_in_lines(matrix, n);
 find_min_in_parallel_diagonals(matrix, n);
 
 deleting_matrix(matrix, n);
+}
+catch(const std::string& message) {
+   std::cerr << message << std::endl;
+   std::exit(1);
+}
     return 0;
 }
