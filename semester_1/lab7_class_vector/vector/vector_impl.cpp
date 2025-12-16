6c253e6453dc8c957f93719e993d421778a2d62c
Kotlyarova555 <85682978+Kotlyarova555@users.noreply.github.com>
2025-12-16 20:17:08 +0300

    Update vector_impl.cpp
    dz

--- semester_1/lab7_class_vector/vector/vector_impl.cpp
+++ semester_1/lab7_class_vector/vector/vector_impl.cpp
@@ -1,3 +1,132 @@
 #include "vector_impl.h"
-
 // your code goes here
+
+#include <stdexcept>
+#include <algorithm>
+
+Vector::Vector() = default;
+
+Vector::Vector(size_t size) : size_(size), capacity_(size) {
+    if (size_ > 0) {
+        data_ = new int[size_];
+        for (size_t i = 0; i < size_; i++) {
+            data_[i] = 0;
+        }
+    }
+}
+
+Vector::Vector(std::initializer_list<int> list) : size_(list.size()), capacity_(list.size()) {
+    if (size_ > 0) {
+        data_ = new int[size_];
+        size_t i = 0;
+        for (int value : list) {
+            data_[i] = value;
+            i++;
+        }
+    }
+}
+
+Vector::Vector(const Vector& other) : size_(other.size_), capacity_(other.capacity_) {
+    if (size_ > 0) {
+        data_ = new int[capacity_];
+        for (size_t i = 0; i < size_; i++) {
+            data_[i] = other.data_[i];
+        }
+    }
+}
+
+Vector& Vector::operator=(const Vector& other) {
+    if (this != &other) {
+        Vector temp(other);
+        Swap(temp);
+    }
+    return *this;
+}
+
+Vector::~Vector() {
+    delete[] data_;
+}
+
+void Vector::Swap(Vector& other) {
+    std::swap(data_, other.data_);
+    std::swap(size_, other.size_);
+    std::swap(capacity_, other.capacity_);
+}
+
+int& Vector::operator[](size_t index) {
+    return data_[index];
+}
+
+const int& Vector::operator[](size_t index) const {
+    return data_[index];
+}
+
+int& Vector::At(size_t index) {
+    if (index >= size_) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data_[index];
+}
+
+const int& Vector::At(size_t index) const {
+    if (index >= size_) {
+        throw std::out_of_range("Index out of range");
+    }
+    return data_[index];
+}
+
+size_t Vector::Size() const {
+    return size_;
+}
+
+size_t Vector::Capacity() const {
+    return capacity_;
+}
+
+void Vector::PushBack(int value) {
+    if (size_ == capacity_) {
+        size_t new_capacity = (capacity_ == 0) ? 1 : capacity_ * 2;
+        Reserve(new_capacity);
+    }
+    data_[size_] = value;
+    size_++;
+}
+
+void Vector::PopBack() {
+    if (size_ > 0) {
+        size_--;
+    }
+}
+
+void Vector::Clear() {
+    size_ = 0;
+}
+
+void Vector::Reserve(size_t new_capacity) {
+    if (new_capacity <= capacity_) {
+        return;
+    }
+
+    int* new_data = new int[new_capacity];
+
+    for (size_t i = 0; i < size_; i++) {
+        new_data[i] = data_[i];
+    }
+
+    delete[] data_;
+
+    data_ = new_data;
+    capacity_ = new_capacity;
+}
+
+std::ostream& operator<<(std::ostream& os, const Vector& vec) {
+    os << '[';
+    for (size_t i = 0; i < vec.size_; i++) {
+        os << vec.data_[i];
+        if (i != vec.size_ - 1) {
+            os << ", ";
+        }
+    }
+    os << ']';
+    return os;
+}
