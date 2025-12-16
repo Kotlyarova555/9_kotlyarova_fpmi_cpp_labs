83c102eee9ec0ebaf35d0a4d68fb1ef6bad00832
Kotlyarova555 <85682978+Kotlyarova555@users.noreply.github.com>
2025-12-16 20:16:07 +0300

    Update vector_impl.h
    complete the task

--- semester_1/lab7_class_vector/vector/vector_impl.h
+++ semester_1/lab7_class_vector/vector/vector_impl.h
@@ -1,5 +1,42 @@
 #pragma once
-
+#include <iostream>
+#include <initializer_list>
 class Vector {
     // your code goes here
+
+private:
+    int* data_ = nullptr;
+    size_t size_ = 0;
+    size_t capacity_ = 0;
+
+public:
+    
+    Vector();
+    explicit Vector(size_t size);
+    Vector(std::initializer_list<int> list);
+    Vector(const Vector& other);
+
+    Vector& operator=(const Vector& other);
+
+    ~Vector();
+
+    void Swap(Vector& other);
+
+    int& operator[](size_t index);
+    const int& operator[](size_t index) const;
+
+    int& At(size_t index);
+    const int& At(size_t index) const;
+
+    size_t Size() const;
+    size_t Capacity() const;
+
+    void PushBack(int value);
+    void PopBack();
+    void Clear();
+    void Reserve(size_t new_capacity);
+
+    friend std::ostream& operator<<(std::ostream& os, const Vector& vec);
 };
+
+
